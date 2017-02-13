cd0848e665c0c43263ace6b4d06cc4e7061119d7
shtnkgm <nakagami.shota@gmail.com>
2017-02-13 23:29:51 +0900

    first commit

---
+++ UIFeedbackGeneratorSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIFeedbackGeneratorSample
+//
+//  Created by Shota Nakagami on 2017/02/13.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
